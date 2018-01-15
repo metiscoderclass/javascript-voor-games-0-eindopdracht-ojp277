cc5f590230b4850aee00886d19d996bf315e86ff
Otto Plasmeijer <ottojohan.plasmeijer@coderclass.nl>
2018-01-15 09:59:47 +0100

    1.1: Er is een blokje.

---
+++ java.js
@@ -0,0 +1,15 @@
+var breedte;
+var hoogte;
+var xpos;
+var ypos;
+
+function setup(){
+  breedte = 50;
+  hoogte = 50;
+  xpos = 0;
+  ypos = 0;
+}
+
+function draw(){
+  rect(xpos, ypos, breedte, hoogte);
+}
