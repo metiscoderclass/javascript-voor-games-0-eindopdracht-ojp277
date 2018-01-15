cc0483d7db3f6acc81bdde7ca04eb626c91f810f
Otto Plasmeijer <ottojohan.plasmeijer@coderclass.nl>
2018-01-15 10:15:34 +0100

    1.2: Eigen tekening.

--- java.js
+++ java.js
@@ -1,15 +1,28 @@
-var breedte;
-var hoogte;
-var xpos;
-var ypos;
+var xCar = 50; //Meest links boven
+var yCar = 100; //Meest links boven
+var xSize = 200;
+var ySize = 180;
+var wheelSize = 24;
 
-function setup(){
-  breedte = 50;
-  hoogte = 50;
-  xpos = 0;
-  ypos = 0;
+function setup() {
+  createCanvas(xSize, ySize);
+  background(220);
 }
 
-function draw(){
-  rect(xpos, ypos, breedte, hoogte);
-}
+function draw() {
+    noStroke();
+    fill(13, 48, 0);
+    rect(xCar, yCar, 110, 20);
+    rect(xCar + 10, yCar-22, 70, 40);
+    // Wielen
+    strokeWeight(2);
+    fill(12, 66, 66);
+    rect(xCar + 20, yCar + 10, wheelSize, wheelSize);
+    rect(xCar + 15, yCar + 10, wheelSize, wheelSize);
+    rect(xCar + 60, yCar + 10, wheelSize, wheelSize);
+    rect(xCar + 50, yCar + 10, wheelSize, wheelSize);
+    rect(xCar + 40, yCar + 10, wheelSize, wheelSize);
+    stroke(0); //Zwarte weg
+    line(0, yCar + 21 + wheelSize/2, width, yCar + 21 + wheelSize/2);
+    //'width' is de breedte van je canvas
+};
